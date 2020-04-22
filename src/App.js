2802668870342d5149fe7412cfbf35929f5ff973
horaleon1 <horaleon1@gmail.com>
2020-04-22 12:44:28 -0500

    top functions n eject react-app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -14,7 +13,8 @@ class App extends Component {
   render() {
     return (
       <div data-test="component-app">
-       <h1>App</h1>
+        <h1 data-test='counter-display' >The counter is currently</h1>
+        <button data-test='increment-button'></button>
       </div>
     );
   }
