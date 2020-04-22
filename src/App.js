a41a98397e83ccb0130329cf70e8a634c3bfaf53
horaleon1 <horaleon1@gmail.com>
2020-04-22 14:11:30 -0500

    changed logic decrement

--- src/App.js
+++ src/App.js
@@ -7,22 +7,38 @@ class App extends Component {
 
     this.state = {
       counter: 0,
+      message: false
     };
   }
+  decremementCounter = () => {
+    if (this.state.counter <= 0) {
+      this.setState({ message: !this.state.message });
+    } else {
+      this.setState({ counter: this.state.counter - 1 });
+      this.setState({ message: false });
+    }
+  };
   render() {
+    if(this.state.message){
+      return <p style={{ color: 'red' }}>El counter no puede ser menor a 0</p>
+    }
     return (
       <div data-test='component-app'>
         <h1 data-test='counter-display'>
-          The counter is currently{this.state.counter}
+          The counter is currently {this.state.counter}
         </h1>
         <button
           data-test='increment-button'
           onClick={() => this.setState({ counter: this.state.counter + 1 })}
-        >+</button>
+        >
+          +
+        </button>
         <button
           data-test='decrement-button'
-          onClick={() => this.setState({ counter: this.state.counter - 1 })}
-        ></button>
+          onClick={() => this.decremementCounter()}
+        >
+          -
+        </button>
       </div>
     );
   }
